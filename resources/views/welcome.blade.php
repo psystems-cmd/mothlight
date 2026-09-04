9eb805f3332f003e6131cfb277411cfaf62bd445
pascucci <p.systems@proton.me>
2026-09-04 15:50:01 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <body>
+
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; min-height: 100vh;">
+    <!-- next div was created by AI-->
+    <div style="text-align: center; padding-top: 2em;">
+        <pre role="img" aria-label="A moth" style="color: orange; display: inline-block; text-align: left; margin: 0; font-family: ui-monospace, SFMono-Regular, Menlo, Consolas, monospace; font-size: 0.7rem; line-height: 1.15;">
+  ____   ____
+ {  ~ \,/ ~  }
+  \ ~ _|_ ~ /
+   (__/ \__)</pre>
+    </div>
+
+
+
     <h1 style="color: orange; font-family: Arial, sans-serif; padding: 1em; text-align: center;">Welcome,<br>little moth.</h1>
+
+
+</div>
+
 </body>
 </html>
