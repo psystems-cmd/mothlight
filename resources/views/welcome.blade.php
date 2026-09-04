ed976036620bb5e55b13261fbfd7fc943cf32542
pascucci <p.systems@proton.me>
2026-09-04 15:27:36 +0200

    Changed a tiny bit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <body>
-    <h1>Hello my favourite little moth.</h1>
+    <h1 style="color: orange; font-family: Arial, sans-serif; padding: 1em; text-align: center;">Welcome,<br>little moth.</h1>
 </body>
 </html>
