7012fbe0c893846ea951abfd8f1396908a8b5d2c
pascucci <p.systems@proton.me>
2026-09-21 18:39:18 +0200

    Update 2026_09_21_162333_create_dreams_table.php

--- database/migrations/2026_09_21_162333_create_dreams_table.php
+++ database/migrations/2026_09_21_162333_create_dreams_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('dreams', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('is_public')->default(false);
+            $table->foreignId('user_id')->constrained();
+
         });
     }
 
