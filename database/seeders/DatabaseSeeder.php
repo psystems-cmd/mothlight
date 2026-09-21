364d1b413721c197f536faab7ceaf953e4d9ee6a
pascucci <p.systems@proton.me>
2026-09-21 13:48:25 +0200

    Add sample data to "Pattern"
    - Added Factory and Seeder for "Patter"
    - Added Controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Pattern;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        Pattern::factory()->count(5)->create();
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        
     }
 }
